662488176c51526c04dad625eebb9c1ab52d8ab6
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-10-08 14:32:33 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -33,17 +33,14 @@ app.get("/receitas/:id", (req, res) => {
     res.send(receita);
 });
 
-app.post("/receitas", (req, res) => {
-    const { titulo, ingredientes, preparo } = req.body;
+    app.post("/receitas", (req, res) => {
+        const { titulo, ingredientes, preparo } = req.body;
 
-    const novaReceita = {
-        id: receitas.length + 1,
-        titulo: titulo,
-        ingredientes: ingredientes,
-        preparo: preparo
-    }
-    receitas.push(novaReceita);
-    res.send("Deu Certo!!")
-});
+        if (!titulo || !ingredientes || !preparo) return res.status(422).send("Todos os campos são obrigatórios!!");
+
+        const novaReceita = { id: receitas.length + 1, titulo: titulo, ingredientes: ingredientes, preparo: preparo };
+        receitas.push(novaReceita);
+        res.sendStatus(201);
+    });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}.`));
