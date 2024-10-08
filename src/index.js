f4d29382b530cab66363ef8df1c7709b3d543dd6
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-10-08 13:47:54 -0300

    Post da rota receitas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
 const PORT = 4000;
 
 const receitas = [
@@ -18,7 +21,7 @@ const receitas = [
         ingredientes: "Leite, Aveia e Whey",
         preparo: "Misture tudo na panela fervendo",
     }
-]
+];
 
 app.get("/receitas", (req, res) => {
     res.send(receitas);
@@ -28,6 +31,19 @@ app.get("/receitas/:id", (req, res) => {
     const { id } = req.params;
     const receita = receitas.find((item) => item.id === Number(id));
     res.send(receita);
+});
+
+app.post("/receitas", (req, res) => {
+    const { titulo, ingredientes, preparo } = req.body;
+
+    const novaReceita = {
+        id: receitas.length + 1,
+        titulo: titulo,
+        ingredientes: ingredientes,
+        preparo: preparo
+    }
+    receitas.push(novaReceita);
+    res.send("Deu Certo!!")
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}.`));
