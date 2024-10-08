ddf02c2b9d8a1ff9db1ce89ea74bf7cf4dec24b7
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-10-08 12:15:25 -0300

    Rotas e Parametros

--- src/index.js
+++ src/index.js
@@ -5,23 +5,29 @@ const app = express();
 app.use(cors());
 const PORT = 4000;
 
-app.get("/receitas", (req, res) => {
-    const receitas = [
-        {
-            id: 1,
-            titulo: "Pão com Ovo",
-            ingredientes: "Pão e Ovo",
-            preparo: "Frite o ovo e coloque no pão",
-        },
-        {
-            id: 2,
-            titulo: "Mingau de Whey",
-            ingredientes: "Leite, Aveia e Whey",
-            preparo: "Misture tudo na panela fervendo",
-        }
-    ]
+const receitas = [
+    {
+        id: 1,
+        titulo: "Pão com Ovo",
+        ingredientes: "Pão e Ovo",
+        preparo: "Frite o ovo e coloque no pão",
+    },
+    {
+        id: 2,
+        titulo: "Mingau de Whey",
+        ingredientes: "Leite, Aveia e Whey",
+        preparo: "Misture tudo na panela fervendo",
+    }
+]
 
-    res.send(receitas); 
+app.get("/receitas", (req, res) => {
+    res.send(receitas);
+});
+
+app.get("/receitas/:id", (req, res) => {
+    const { id } = req.params;
+    const receita = receitas.find((item) => item.id === Number(id));
+    res.send(receita);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}.`));
