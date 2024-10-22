5cef448d2bde77184760e71184e9c52f80cb5fe4
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-22 07:06:47 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import bcrypt from 'bcrypt';
 import cors from 'cors';
-import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
+import express from 'express';
 import joi from 'joi';
+import { MongoClient, ObjectId } from 'mongodb';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 
@@ -18,6 +20,12 @@ try {
     console.log(err.message);
 }
 const db = mongoClient.db();
+
+const usuarioSchema = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().required().min(3)
+});
 
 app.get("/receitas", async (req, res) => {
     try {
@@ -40,7 +48,12 @@ app.get("/receitas/:id", async (req, res) => {
 });
 
 app.post("/receitas", async (req, res) => {
+    const { authorization } = req.headers;
     const { titulo } = req.body;
+
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) return res.sendStatus(401);
 
     const receitaSchema = joi.object({
         titulo: joi.string().required(),
@@ -56,10 +69,13 @@ app.post("/receitas", async (req, res) => {
     }
 
     try {
+        const sessao = await db.collection("sessoes").findOne({ token });
+        if (!sessao) return res.sendStatus(401);
+
         const recipe = await db.collection("receitas").findOne({ titulo: titulo });
         if (recipe) return res.status(409).send("Essa receita já existe.");
 
-        await db.collection("receitas").insertOne(req.body);
+        await db.collection("receitas").insertOne({ ...req.body, idUsuario: sessao.idUsuario });
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
@@ -80,6 +96,10 @@ app.delete("/receitas/:id", async (req, res) => {
 
 app.put("/receitas/:id", async (req, res) => {
     const { id } = req.params;
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+
+    if (!token) return res.sendStatus(401);
 
     const receitaSchema = joi.object({
         titulo: joi.string(),
@@ -95,12 +115,17 @@ app.put("/receitas/:id", async (req, res) => {
     }
 
     try {
-        const result = await db.collection("receitas").updateOne(
+        const sessao = await db.collection("sessoes").findOne({ token });
+        if (!sessao) return res.sendStatus(401);
+
+        const receita = await db.collection("receitas").findOne({ _id: new ObjectId(id) });
+        if (!receita) return res.sendStatus(404);
+        if (receita.idUsuario !== sessao.idUsuario) return res.sendStatus(401);
+
+        await db.collection("receitas").updateOne(
             { _id: new ObjectId(id) },
             { $set: req.body }
         );
-
-        if (result.matchedCount === 0) return res.status(404).send("Este item não existe");
         res.send("Receita atualizada");
     } catch (err) {
         res.status(500).send(err.message);
@@ -125,6 +150,47 @@ app.put("/receitas/muitas/:filtroIngredientes", async (req, res) => {
         if (result.matchedCount === 0) return res.status(404).send("Não existe nenhuma receita com esse filtro.");
         res.send("receitasEditadas");
 
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/sign-up", async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    const validation = usuarioSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const usuario = await db.collection("usuarios").findOne({ email });
+        if (usuario) return res.status(409).send("E-mail já cadastrado");
+
+        const hash = bcrypt.hashSync(senha, 10);
+
+        await db.collection("usuarios").insertOne({ nome, email, senha: hash });
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/sign-in", async (req, res) => {
+    const { email, senha } = req.body;
+
+    try {
+        const usuario = await db.collection("usuarios").findOne({ email });
+        if (!usuario) return res.status(401).send("E-mail não cadastrado");
+
+        const correctPassword = bcrypt.compareSync(senha, usuario.senha);
+        if (!correctPassword) return res.status(401).send("Senha incorreta");
+
+        const token = uuid();
+        await db.collection("sessoes").insertOne({ token, idUsuario: usuario._id });
+        req.send(token);
+        // res.sendStatus(200);
     } catch (err) {
         res.status(500).send(err.message);
     }
